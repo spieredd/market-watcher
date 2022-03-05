e2eeea753d239860c3360b8386568daf46125b4a
science-math-guy <adumont@mailbox.org>
2022-03-05 13:27:44 +0100

    First MVP

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,21 +3,28 @@ import React from 'react'
 
 function header() {
   return (
-    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+    <header className="mx-auto flex max-w-7xl justify-between p-5">
       <div className="flex items-center space-x-5">
-            <Link href="/">
-                  <img className="w-44 object-contain cursor-pointer" src="https://upload.wikimedia.org/wikipedia/en/6/67/Medium_logo_%282020%29.png" alt="" />
-              </Link>
-              <div className="hidden md:inline-flex items-center space-x-5">
-                  <h3>About</h3>
-                  <h3>Contact</h3>
-                  <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
-              </div>
+        <Link href="/">
+          <img
+            className="w-44 cursor-pointer object-contain"
+            src="https://upload.wikimedia.org/wikipedia/en/6/67/Medium_logo_%282020%29.png"
+            alt=""
+          />
+        </Link>
+        <div className="hidden items-center space-x-5 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
+        </div>
       </div>
-          <div className="flex items-center space-x-5 text-green-600">
-            <h3>Sign In</h3>
-              <h3 className="border px-4 py-1 rounded-full border-green-600">
-                  Get Started</h3>  
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="rounded-full border border-green-600 px-4 py-1">
+          Get Started
+        </h3>
       </div>
     </header>
   )
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,10 +1,60 @@
 import {sanityClient,urlFor} from '../../sanity'
 import { Post } from '../../typings';
+import Header from '../../components/Header'
+import { GetStaticProps } from 'next';
+import PortableText from 'react-portable-text';
 
-function Post() {
+interface Props{
+    post: Post;
+}
+
+function Post({ post }: Props) {
   return (
       <main>
           <Header />
+          <img className="w-full h-40 object-cover" src={urlFor(post.mainImage)} alt="" />
+          <article className="max-w-3xl m-auto p-5">
+              <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+              <h2 className="text-xl font-light text-gray-500 mb-2">
+                  {post.description}
+              </h2>
+              <div className="flex items-center space-x-2">
+                  <img
+                      className="h-10 w-10 rounded-full"
+                      src={urlFor(post.author.image).url()!}
+                      alt=""
+                  />
+                  <p className="font-extralight">
+                      Blog post by{" "}
+                      <span className="text-green-600">{post.author.name}</span> - 
+                      Published at {new Date(post._createdAt).toLocaleString()}
+                  </p>
+              </div>
+              <div className="my-10">
+                  <PortableText
+                      className=''
+                      dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                      projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+                      content={post.body}
+                      serializers={{
+                          h1: (props: any) => {
+                              <h1 className="text-2xl font-bold my-5" {...props} />
+                          },
+                          h2: (props: any) => {
+                              <h1 className="text-xl font-bold my-5" {...props} />
+                          },
+                          li: ({ children }: any) => {
+                              <li className="ml-4 list-disc">{children}</li>
+                          },
+                          link: ({ href, children }: any) => {
+                              <a href={href} className="text-blue-500 hover:underline">
+                                  {children}
+                              </a>
+                          },
+                      }}
+                  />
+              </div>
+          </article>
     </main>
   )
 }
@@ -29,5 +79,44 @@ export const getStaticPaths = async () => {
     return {
         paths,
         fallback: "blocking"
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const query = `
+    *[_type == "post" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        title,
+        author->{
+            name,
+            image
+        },
+        'comments':*[
+            _type == 'comment' &&
+            post._ref == ^.id &&
+            approved == true
+        ],
+        description,
+        mainImage,
+        slug,
+        body
+    }
+    `
+    const post = await sanityClient.fetch(query,{
+        slug: params?.slug
+    });
+
+    if (!post) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            post,
+        },
+        revalidate:60,
     }
 }
