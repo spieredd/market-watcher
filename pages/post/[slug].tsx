3f7d0d88c318c6757aa23b1f685824a0e0cd0c9c
science-math-guy <adumont@mailbox.org>
2022-03-06 22:43:48 +0100

    Head

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header'
 import { GetStaticProps } from 'next'
 import PortableText from 'react-portable-text'
 import Link from 'next/link'
+import Head from 'next/head'
 
 interface Props {
   post: Post
@@ -11,86 +12,92 @@ interface Props {
 
 function Post({ post }: Props) {
   return (
-    <main>
-      <header className="mx-auto flex max-w-7xl justify-between p-5">
-        <div className="flex items-center space-x-5">
-          <Link href="/">
+    <div>
+      <Head>
+        <title>Market Watcher - Economic News</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <header className="mx-auto flex max-w-7xl justify-between p-5">
+          <div className="flex items-center space-x-5">
+            <Link href="/">
+              <img
+                className="h-10 w-44 cursor-pointer object-cover"
+                src="../logo-market-watcher.png"
+                alt=""
+              />
+            </Link>
+            <div className="hidden items-center space-x-5 md:inline-flex">
+              <Link href="/">
+                <h3 className="cursor-pointer">Home</h3>
+              </Link>
+              <Link href="/about">
+                <h3 className="cursor-pointer">About</h3>
+              </Link>
+              <h3>Contact</h3>
+              <Link href="https://www.buymeacoffee.com/marketwatcher">
+                <h3 className="cursor-pointer rounded-full bg-green-600 px-4 py-1 text-white">
+                  Buy me a coffee
+                </h3>
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center space-x-5 text-green-600">
+            <h3>Sign In</h3>
+            <h3 className="rounded-full border border-green-600 px-4 py-1">
+              Get Started
+            </h3>
+          </div>
+        </header>{' '}
+        <img
+          className="h-40 w-full object-cover"
+          src={urlFor(post.mainImage).url()!}
+          alt=""
+        />
+        <article className="m-auto max-w-3xl p-5">
+          <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+          <h2 className="mb-2 text-xl font-light text-gray-500">
+            {post.description}
+          </h2>
+          <div className="flex items-center space-x-2">
             <img
-              className="h-10 w-44 cursor-pointer object-cover"
-              src="../logo-market-watcher.png"
+              className="h-10 w-10 rounded-full"
+              src={urlFor(post.author.image).url()!}
               alt=""
             />
-          </Link>
-          <div className="hidden items-center space-x-5 md:inline-flex">
-            <Link href="/">
-              <h3 className="cursor-pointer">Home</h3>
-            </Link>
-            <Link href="/about">
-              <h3 className="cursor-pointer">About</h3>
-            </Link>
-            <h3>Contact</h3>
-            <Link href="https://www.buymeacoffee.com/marketwatcher">
-              <h3 className="cursor-pointer rounded-full bg-green-600 px-4 py-1 text-white">
-                Buy me a coffee
-              </h3>
-            </Link>
+            <p className="font-extralight">
+              Blog post by{' '}
+              <span className="text-green-600">{post.author.name}</span> -
+              Published at {new Date(post._createdAt).toLocaleString()}
+            </p>
           </div>
-        </div>
-        <div className="flex items-center space-x-5 text-green-600">
-          <h3>Sign In</h3>
-          <h3 className="rounded-full border border-green-600 px-4 py-1">
-            Get Started
-          </h3>
-        </div>
-      </header>{' '}
-      <img
-        className="h-40 w-full object-cover"
-        src={urlFor(post.mainImage).url()!}
-        alt=""
-      />
-      <article className="m-auto max-w-3xl p-5">
-        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
-        <h2 className="mb-2 text-xl font-light text-gray-500">
-          {post.description}
-        </h2>
-        <div className="flex items-center space-x-2">
-          <img
-            className="h-10 w-10 rounded-full"
-            src={urlFor(post.author.image).url()!}
-            alt=""
-          />
-          <p className="font-extralight">
-            Blog post by{' '}
-            <span className="text-green-600">{post.author.name}</span> -
-            Published at {new Date(post._createdAt).toLocaleString()}
-          </p>
-        </div>
-        <div className="my-10">
-          <PortableText
-            className=""
-            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
-            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-            content={post.body}
-            serializers={{
-              h1: (props: any) => {
-                ;<h1 className="my-5 text-2xl font-bold" {...props} />
-              },
-              h2: (props: any) => {
-                ;<h1 className="my-5 text-xl font-bold" {...props} />
-              },
-              li: ({ children }: any) => {
-                ;<li className="ml-4 list-disc">{children}</li>
-              },
-              link: ({ href, children }: any) => {
-                ;<a href={href} className="text-blue-500 hover:underline">
-                  {children}
-                </a>
-              },
-            }}
-          />
-        </div>
-      </article>
-    </main>
+          <div className="my-10">
+            <PortableText
+              className=""
+              dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+              projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+              content={post.body}
+              serializers={{
+                h1: (props: any) => {
+                  ;<h1 className="my-5 text-2xl font-bold" {...props} />
+                },
+                h2: (props: any) => {
+                  ;<h1 className="my-5 text-xl font-bold" {...props} />
+                },
+                li: ({ children }: any) => {
+                  ;<li className="ml-4 list-disc">{children}</li>
+                },
+                link: ({ href, children }: any) => {
+                  ;<a href={href} className="text-blue-500 hover:underline">
+                    {children}
+                  </a>
+                },
+              }}
+            />
+          </div>
+        </article>
+      </main>
+    </div>
   )
 }
 
